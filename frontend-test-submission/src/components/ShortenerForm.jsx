5f0e1ea19ce22c289fc294251bbe9808c46a2ba1
Mohd Faizaan <mohdfaizaan35@gmail.com>
2025-08-05 12:01:03 +0530

    fixed some import issues

--- frontend-test-submission/src/components/ShortenerForm.jsx
+++ frontend-test-submission/src/components/ShortenerForm.jsx
@@ -9,7 +9,7 @@ import {
   IconButton,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import { log } from "../utils/log";
+import { log } from "../utils/logger";
 
 const ShortenerForm = ({ onSubmit }) => {
   const [urls, setUrls] = useState([
